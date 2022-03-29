7a5fcf90ca19cd795568c1a455fb1fb6604eb762
victorHL99 <victor.lessa@estudante.ufla.br>
2022-03-29 16:09:10 -0300

    Tela de Registro Implementada

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,5 +1,60 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+import Logo from "./../../assets/img/logo.png";
+
+
+
 export default function Register(){
     return (
-        <p> outro teste</p>
+        <Main>
+            <LogoCSS src={Logo} alt="Logo do TrackIt"/>
+            <Input placeholder="Email"></Input>
+            <Input placeholder="Senha"></Input>
+            <Input placeholder="Nome"></Input>
+            <Input placeholder="Foto"></Input>
+
+            <Button>Cadastrar</Button>
+            <Link to="/">Já tem uma conta? Faça login!</Link>
+
+
+        </Main>
     )
 }
+
+const Main = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction:column;
+    align-items:center;
+    padding: 0;
+    margin-top:68px;
+    
+`
+    
+const LogoCSS = styled.img`
+    width: 180px;
+    height: 178.83px;
+    margin-bottom:32.62px
+    
+`
+const Input = styled.input`
+    width: 303px;
+    height: 45px;
+    margin-bottom: 6px;
+    border: 1px solid #D5D5D5;
+    border-radius: 5px;
+    cursor: pointer;
+`
+
+const Button = styled.button`
+    width: 303px;
+    height: 45px;
+    border-radius: 4.64px;
+    margin-bottom: 25px;
+    cursor: pointer;
+`
+
+
